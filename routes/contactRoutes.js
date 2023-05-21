8401c7944d773af50d8a312c66e96cee4e6d8414
tuong.nguyent <tntt112d@spgroup.com.sg>
2023-05-21 09:17:12 +0700

    Fix wrong syntax

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,19 +2,19 @@ import express, { Router } from 'express';
 
 var router = express.Router();
 
-router.route("/", (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({message: "Get all contacts"});    
 })
 
-router.route("/", (req, res) => {
+router.post('/', (req, res) => {
     res.status(200).json({message: "Create contact"});
 }) 
 
-router.route("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
     res.status(200).json({message: `Update contact for ${req.params.id}`});
 })
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(200).json({message: `Delete contact ${req.params.id}`});
 })
 
