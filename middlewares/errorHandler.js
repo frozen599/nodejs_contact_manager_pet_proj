dc2e612ac7a2e4ae93ecdf4e2aae2d5682fced84
tuong.nguyent <tntt112d@spgroup.com.sg>
2023-05-21 16:34:54 +0700

    New error handler and status codes

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,19 +1,39 @@
+import {BAD_REQUEST, UNAUTHORIZED, FORBIDDEN, NOT_FOUND,INTERNAL_SERVER_ERROR, } from '../constants.js'
+
 const errHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode: 500;
     switch (statusCode) {
-        case 400:
+        case BAD_REQUEST:
             res.json({
                 title: "Validation failed",
                 message: err.message,
                 stackTrace: err.stack
             })
-        case 404:
+        case NOT_FOUND:
             res.json({
                 title: "Not found",
                 message: err.message,
                 stackTrace: err.stack,
             })
             break;
+        case UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack,
+            })
+        case FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack,
+            })
+        case INTERNAL_SERVER_ERROR:
+            res.json({
+                title: "Internal Server Error",
+                message: err.message,
+                stackTrace: err.stack,
+            })
         default:
             break;
     }
