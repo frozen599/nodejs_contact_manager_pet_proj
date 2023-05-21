bbf441fe5c431eeffc600d010c481f6ccee33000
tuong.nguyent <tntt112d@spgroup.com.sg>
2023-05-21 16:23:00 +0700

    Edit error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,22 @@
 const errHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode: 500;
+    switch (statusCode) {
+        case 400:
+            res.json({
+                title: "Validation failed",
+                message: err.message,
+                stackTrace: err.stack
+            })
+        case 404:
+            res.json({
+                title: "Not found",
+                message: err.message,
+                stackTrace: err.stack,
+            })
+            break;
+        default:
+            break;
+    }
     res.json({message: err.message, stack_trace: err.stack}); 
 };
 
